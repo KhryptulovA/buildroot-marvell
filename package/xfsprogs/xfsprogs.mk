2f983628452786f9bf101ad1a69cb6da53f5ffc8
KhryptulovA <67318045+KhryptulovA@users.noreply.github.com>
2021-01-18 13:49:16 +0200

    Update xfsprogs.mk
    change URL

--- package/xfsprogs/xfsprogs.mk
+++ package/xfsprogs/xfsprogs.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 XFSPROGS_VERSION = 3.1.11
-XFSPROGS_SITE = ftp://oss.sgi.com/projects/xfs/cmd_tars
+#XFSPROGS_SITE = ftp://oss.sgi.com/projects/xfs/cmd_tars
+XFSPROGS_SITE = ftp.osuosl.org/pub/blfs/conglomeration/xfsprogs/
 
 XFSPROGS_DEPENDENCIES = util-linux
 
